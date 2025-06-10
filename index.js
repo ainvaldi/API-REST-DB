739a041a2d55dbb5e5502f8c1761005feb12d195
Agustin-Invaldi <agustin.invaldi@emprinet.com>
2025-06-09 23:03:02 -0300

    add-module-sales

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ app.use(express.json())
 
 const productRouter = require('./routes/products.routes')
 const usuariosRouter = require('./routes/users.routes');
+const ventasRouter = require('./routes/sales.routes')
 
 app.use('/productos', productRouter)
 app.use('/usuarios', usuariosRouter);
+app.use('/ventas', ventasRouter);
 
 app.listen(port, () => {
     console.log(`servidor corriendo en localhost:${port}`)
