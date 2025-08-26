d980098e37c94c28b8ad4774d8e210e52c90ab8a
Agustin Invaldi <agustin.invaldi@emprinet.com>
2025-08-26 19:38:31 -0300

    add-send-emails

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { register, login } = require('../controllers/auth.controller')
+const { register, login, forgotPassword, resetPassword } = require('../controllers/auth.controller')
 
 router.post('/register', register)
 router.post('/login', login)
+router.post('/forgotPassword', forgotPassword)
+router.post('/resetPassword', resetPassword)
+
 
 module.exports = router
